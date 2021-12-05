5426666f72dd0d14dabbaa55c6cff35b8f40ee04
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2021-12-05 23:36:50 +0300

    fix canvases, add info msg to settings
    Co-Authored-By: Oleja <91268564+asayke@users.noreply.github.com>

---
+++ Assets/Scripts/Buttons/SettingsMenu/ControllingButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Buttons.SettingsMenu
+{
+    public class ControllingButton : MonoBehaviour,IButton
+    {
+        [SerializeField] private GameObject notimplemented;
+        public void OnClick()
+        {
+            notimplemented.SetActive(true);
+            Invoke("DisableText",3.0f);
+        }
+
+        public void DisableText()
+        {
+            notimplemented.SetActive(false);
+        }
+
+
+    }
+}
