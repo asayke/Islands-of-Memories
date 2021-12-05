fee1aa830b38f6e5f67ca40def4d8f2c9d4d452a
Oleja <kolodiazhnyi@sfedu.ru>
2021-12-05 23:03:23 +0300

    SoundSlider update(final)
    thats working
    
    Co-Authored-By: lfybbk10 <63192458+lfybbk10@users.noreply.github.com>

---
+++ Assets/Scripts/Sliders/SoundSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sliders
+{
+    public class SoundSlider : MonoBehaviour 
+    {
+        [SerializeField] private Slider slider;
+        [SerializeField] private GameObject camera;
+
+        private void Update()
+        {
+            camera.GetComponent<AudioSource>().volume = slider.value;
+        }
+    }
+}
