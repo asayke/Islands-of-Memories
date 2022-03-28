01e0e651e57c52fae9e268540ff22d9d276f921e
Oleja <kolodiazhnyi@sfedu.ru>
2022-03-28 18:18:34 +0300

    prePlayerStats
    Co-Authored-By: lfybbk10 <63192458+lfybbk10@users.noreply.github.com>

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Rendering;
+
+namespace Player
+{
+    public class Stat
+    {
+        public float Value { get; private set; }
+
+        public Stat(float value)
+        {
+            Value = value;
+        }
+        public void IncStat(float additional)
+        {
+            Value += additional;
+        }
+
+    }
+
+    public class PlayerStats
+    {
+        private Stat Strength { get; set; }
+        private Stat Agility { get; set; }
+        private Stat Intelligence { get; set; }
+        private Stat Stamina { get; set; }
+
+        public PlayerStats(float strength, float stamina, float agility, float intelligence)
+        {
+            Strength = new Stat(strength);
+            Agility = new Stat(agility);
+            Intelligence = new Stat(intelligence);
+            Stamina = new Stat(stamina);
+        }
+
+        
+        
+    }
+}
