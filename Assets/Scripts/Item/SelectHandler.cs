1a6989bd0ab0c6b84a429cf6c69d21fbd8230866
Oleja <kolodiazhnyi@sfedu.ru>
2022-03-30 21:50:58 +0300

    Take Item System
    Система оповещений о возможности подбора, его реализация
    
    Co-Authored-By: lfybbk10 <63192458+lfybbk10@users.noreply.github.com>

--- Assets/Scripts/Item/SelectHandler.cs
+++ Assets/Scripts/Item/SelectHandler.cs
@@ -1,11 +1,45 @@
 using System;
+using System.Linq;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SelectHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _prompts;
     [SerializeField] private UIInventory _inventory;
     [SerializeField] private CampFire _campFire;
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private Transform _player;
+    private Collider[] _findedColliders = new Collider[50];
+    [SerializeField] private GameObject _text;
+
+    private void FixedUpdate()
+    {
+        var count = Physics.OverlapSphereNonAlloc(_player.position, 6f, _findedColliders, _layerMask);
+        if (count > 0)
+        {
+            Collider nearlesObj = _findedColliders[0];
+            _text.transform.position = new Vector3(nearlesObj.transform.position.x,nearlesObj.transform.position.y + 1f, nearlesObj.transform.position.z);
+            _text.transform.DOScale(new Vector3(-1, 1, 1), 0.15f);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                var dropItem = nearlesObj.GetComponent<DropItem>();
+                 _inventory.Inventory.TryToAdd(new Item(dropItem.ItemInfo,1));
+                var parent = nearlesObj.transform.parent;
+                if (parent != null)
+                    Destroy(parent.gameObject);
+                _text.transform.DOScale(new Vector3(0, 0, 0), 0.15f);
+                Destroy(nearlesObj.transform.gameObject);
+            }
+        }
+        else
+        {
+            _text.transform.DOScale(new Vector3(0, 0, 0), 0.15f); 
+        }
+    }
+
+
     // private void Update()
     // {
     //     //Часть для вызова подсказки на забор предмета.
